6e18ab4c63a44cc0813904d559c6b8424f442ccb
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-08-17 12:56:42 +0200

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker___Asp.Net_Core_MVC.Models
+{
+	public class Category
+	{
+		[Key]
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string Title { get; set; }
+
+		[Column(TypeName = "nvarchar(50)")]
+
+		public string Icon { get; set; } = "";
+		[Column(TypeName = "nvarchar(50)")]
+		public string Type { get; set; } = "Expense";
+    }
+}
