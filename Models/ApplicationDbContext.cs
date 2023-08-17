b73cb6650e8c785d3eaae4af353d7b00e93baf47
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-08-17 13:22:07 +0200

    Nieuwe database migratie aanmaken in EF

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker___Asp.Net_Core_MVC.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
